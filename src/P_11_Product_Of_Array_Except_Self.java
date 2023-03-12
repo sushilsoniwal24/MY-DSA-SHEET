23b2cdab75900664a289a0b11e2e60e8949267e4
Sushil Soniwal <soniwalsushil24@gmail.com>
2023-03-12 15:18:25 +0530

    LC-238:- Product of Array Except Self

---
+++ src/P_11_Product_Of_Array_Except_Self.java
@@ -0,0 +1,78 @@
+import java.util.Arrays;
+
+public class P_11_Product_Of_Array_Except_Self {
+
+//    https://leetcode.com/problems/product-of-array-except-self/description/
+
+    // ? T.C = O(n^2) & S.C = O(1) {TLE}.
+    public static int[] productExceptSelf1(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            int pro = 1;
+            for (int j = 0; j < n; j++) {
+                if (i != j)
+                    pro *= nums[j];
+            }
+            ans[i] = pro;
+        }
+        return ans;
+    }
+
+    // * T.C = O(n) & S.C = O(1) {Not Work For 0}.
+    public static int[] productExceptSelf2(int[] nums) {
+        int n = nums.length, pro = 1;
+        int[] ans = new int[n];
+        for (int num : nums) {
+            pro *= num;
+        }
+        for (int i = 0; i < n; i++) {
+            ans[i] = pro / nums[i];
+        }
+        return ans;
+    }
+
+    // * T.C = O(n) & S.C = O(n).
+    public static int[] productExceptSelf3(int[] nums) {
+        int n = nums.length;
+        int[] pre_pro = new int[n];
+        pre_pro[0] = 1;
+        for (int i = 1; i < n; i++) {
+            pre_pro[i] = pre_pro[i - 1] * nums[i - 1];
+        }
+
+        int[] suf_pro = new int[n];
+        suf_pro[n - 1] = 1;
+        for (int i = n - 2; i >= 0; --i) {
+            suf_pro[i] = suf_pro[i + 1] * nums[i + 1];
+        }
+
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = pre_pro[i] * suf_pro[i];
+        }
+        return ans;
+    }
+
+    // ! T.C = O(n) & S.C = O(1).
+    public static int[] productExceptSelf4(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        ans[0] = 1;
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+        int temp = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            ans[i] = ans[i] * temp;
+            temp *= nums[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 1, 0, -3, 3};
+        int[] res = productExceptSelf4(nums);
+        System.out.println(Arrays.toString(res));
+    }
+}
