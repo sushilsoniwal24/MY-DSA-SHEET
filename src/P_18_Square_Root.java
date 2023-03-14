d1e631f7b38adcc325fb517e512c75414ebc3ea5
Sushil Soniwal <soniwalsushil24@gmail.com>
2023-03-14 08:12:21 +0530

    LC-167:- Two Sum II - Input Array Is Sorted

---
+++ src/P_18_Square_Root.java
@@ -0,0 +1,75 @@
+public class P_18_Square_Root {
+
+    // https://leetcode.com/problems/sqrtx/description/
+
+    // ? T.C = O(n) & S.C = O(1).
+    public static int mySqrt1(int x) {
+        if (x < 2) return x;
+        int ans = 1;
+        for (int i = 2; i <= x / 2; i++) {
+            if (i <= x / i) {
+                ans = i;
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+
+    // * T.C = O(logn) & S.C = O(1).
+    public static int mySqrt2(int x) {
+        if (x < 2) {
+            return x;
+        }
+        int s = 2, e = x / 2;
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+            long square = (long) m * m;
+            if (square == x) {
+                return m;
+            } else if (square > x) {
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+        }
+        return e;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt2(17));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
