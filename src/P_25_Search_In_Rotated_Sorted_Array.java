eb19b94a45862f8c67aa9ba716db03e0282f42b9
Sushil Soniwal <soniwalsushil24@gmail.com>
2023-03-15 11:48:50 +0530

    LC-33:- Search in Rotated Sorted Array

---
+++ src/P_25_Search_In_Rotated_Sorted_Array.java
@@ -0,0 +1,53 @@
+public class P_25_Search_In_Rotated_Sorted_Array {
+
+    // https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+    // ? T.C = O(n) & S.C = O(1).
+    public static int search1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target)
+                return i;
+        }
+        return -1;
+    }
+
+    // * T.C = O(log n) & S.C = O(1).
+    public static int search2(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // If left half of the array is sorted
+            else if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            // If right half of the array is sorted
+            else {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        // If element is not found
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(search2(arr, 1));
+    }
+}
